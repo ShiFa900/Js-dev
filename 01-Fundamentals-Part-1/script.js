7483cb32d058d5b95e071f6dfd4d60edc87929e0
Shifa <luh.shifa@gmail.com>
2024-03-06 09:41:13 +0800

    first commit

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,47 @@
+let js = "shifa";
+// if(js === 'shifa') alert("Hello world, this is Shifa's first JS practice");
+
+console.log(20+394);
+
+// ini adalah salah satu cara untuk men-deklarasikan variable
+// gunakan 'let' untuk membuat variable
+// nama variable = camel case
+let firstName = "Hippy";
+// value dari console ini akan sama dengan value dari variable firstName
+console.log(firstName);
+// typeof disini akan mereturn type data dari variable yang menjadi paramnya
+console.log(typeof true);
+// assigned value baru (meng-update valuenya), tanpa menggunakan 'let' lagi
+firstName = "Ho oh";
+
+// gunakan const untuk menetapkan value dari sebuah variable, valuenya tidak akan bisa diubah, contoh tahun lahir
+// dan const tidak boleh 'empty value'
+const birthDate = 2004;
+
+// type variable ini hampir mirip dengan 'let' tapi berbeda untuk di browser
+var jobs = "koki";
+
+// membuat variable tanpa mendeklarasikan type variablenya, yang mana artinya JS akan menjadikan variable ini global di dalam scope file
+lastName = "Moca";
+
+let now = 2024;
+const ageShifa = now - 2005;
+const ageKiwi = now - 2023;
+
+// gunakan koma (,) untuk separate antara satu value dengan value yang lain
+console.log(ageKiwi, ageShifa);
+
+// gunakan ** double star untuk menulis pangkat
+console.log(2**3);
+
+let namaDepan = "hari";
+let namaBelakang = "selasa";
+
+// concat dua nama
+console.log(namaDepan + " " + namaBelakang);
+
+let x = 12 + 4; // ini 16
+x += 5 // x = x + 5 = 21
+
+console.log(x);
+
