00ba53bde37268e87a1d1f5140435b03dbf53bd8
Shifa <luh.shifa@gmail.com>
2024-03-27 18:23:25 +0800

    Wednesday, Mar 27 2024

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -27,7 +27,7 @@ function calcAge(birthYear) {
 }
 
 
-const firstName = 'Masala';
+// const firstName = 'Masala';
 // calcAge(2005);
 
 // Hoisting variables
@@ -53,37 +53,138 @@ const addExp = function (a, b) {
 
 const addArrow = (a, b) => a + b;
 
-console.log(this);
+// THIS KEYWORD
+// console.log(this); // global object with this keyword
 const hitungAge = function (birthYear) {
     console.log(2024 - birthYear);
-    console.log(this); //
+    console.log(this); // ini undefined, this keywordnya punya sendiri
 }
-hitungAge(2004); // ini akan undefined
+// hitungAge(2004); // ini akan undefined
 
 const hitungAgeArrow = birthYear => {
     console.log(2024 - birthYear);
-    console.log(this); //
+    console.log(this); // ini akan menghasilkan 'window', arror function tidak memiliki this keywordnya sendiri, melainkan global object, this keyword dari global parent
 }
-hitungAgeArrow(2004);
+// hitungAgeArrow(2004);
 
 // ini adalah method coba
 const coba = {
     tahun: 1989,
-    calcAge: function (){
-        console.log(this); // this keyword yang ada disini akan menjadi object yang memanggil si method
-        console.log(2024 - this.tahun);
+    calcAge: function () {
+        // console.log(this); // this keyword yang ada disini akan menjadi object yang memanggil si method
+        // console.log(2024 - this.tahun); // akan mendapatkan hasil dari pengurangan dari tahun sekarang - object tahun
     },
 };
 //memanggil function yang ada pada method coba
-coba.calcAge();
+// coba.calcAge();
 
 const matilda = {
     year: 2018
 };
 
 // ini ndk mau dia, entah apa
-const f = coba.calcAge();
-matilda.calcAge = coba.calcAge();
-console.log(matilda);
+// matilda.calcAge = coba.calcAge();
+// console.log(matilda);
 // ini disebut dengan method borrowing, kita meminjam method jadi tanpa menuliskannya lagi
-matilda.calcAge();
+// matilda.calcAge();
+const f = coba.calcAge();
+// f(); // ini akan menghasilkan undefined, karena adalah sebuah regular function
+
+// REGULAR function VS. ARROW function
+
+var firstName = 'Maya'; // ini adalah global scope, karena menggunakan var
+const babangJonas = {
+    year: 2001,
+    firstName: 'Bang Jonas',
+    calcAge: function () {
+        console.log(this);
+        console.log(2024 - this.year);
+
+        // #SOLUTION 1
+        // const self = this; // menyimpan this keyword, agar bisa digunakan di method isMillenial
+        // const isMillenial = function (){
+        //     console.log(self) // ini isinya akan menjadi object this keyword
+        //     // console.log(this) // ini akan menghasilkan undefined
+        //
+        //     // console.log(this.year >= 1997 && this.year <= 2012);
+        //     console.log(self.year >= 1997 && self.year <= 2012);
+        // }
+        // isMillenial(); // ini akan menghasilkan undefined
+
+        // #SOLUTION 2
+        // arrow function mendapatkan this keyword dari scope parentnya (calcAge), maka dari itu this keyword bisa digunakan oleh arrow function
+        const isMillenial =  () => {
+            console.log(this);
+            console.log(this.year >= 1997 && this.year <= 2012);
+        }
+        isMillenial();
+
+        // *note: di dalam regular function this keyword akan menghasilkan undefined, mendapatkan this keywordnya sendiri (?)
+    },
+
+    // jika ada variable yang menggunakan nama firstName dan menggunakan deklarasi var, maka value dari global variable tersebut akan di print
+    // *note: jangan gunakan arrow function sebagai sebuah method
+    // greet: () => console.log(`Hey ${this.firstName}`) // ini adalah arrow function, dia akan menggunakan this keyword dari global
+    greet: function () {
+        console.log(this);
+        console.log(`Hey ${this.firstName}`); // akan mereturn firstName dari object yang memanggil method greet
+    }
+};
+// babangJonas.greet();
+// babangJonas.calcAge();
+
+// ARGUMENT KEYWORD
+// keyword ini hanya bisa digunakan untuk regular function
+
+// regular function
+const addExpr = function (a,b)
+{
+    console.log(arguments); // ini akan menampilkan argument" yang dimiliki oleh si function
+    return a + b;
+}
+
+// addExpr(4,2);
+// addExpr(4,2, 3, 5); // walau paramnya hanya dua, namun param argumentnya tetap bisa diberi banyak, karena pada function paramnya tidak memiliki nama.
+
+// arrow function
+// const addArrow = (a,b) => {
+//     console.log(arguments); // nah ini akan error, karena argument keyword hanya bisa untuk di regular function
+//     return a + b;
+// }
+// addArrow(4,6);
+
+//PRIMITIVE & REFERENCE OBJECT
+
+// Primitive
+let lastName = 'Wellberg';
+let oldLastName = lastName;
+lastName = 'Willfouw';
+console.log(lastName, oldLastName);
+
+// object reference
+const jamela = {
+    firstName: 'Jamela',
+    lastName: 'Marckeinzie',
+    age: 24
+};
+
+const marriedJamela = jamela;
+// hasilnya tidak akan seperti di primitive
+// hal ini karena di call stack valuenya berupa reference ke memory heap, maka jika object di heap memory ini diganti,
+// maka call stack yang valuenya adalah address reference ke heap akan ikut berubah
+marriedJamela.lastName = jamela;
+console.log('before marriage: ',jamela);
+console.log('after mariage: ', marriedJamela);
+
+// copying object
+const jamela2 = {
+    firstName: 'Jamela',
+    lastName: 'Marckeinzie',
+    age: 24
+};
+
+// function ini fungsinya untuk merge
+const jamelaCopy = Object.assign({}, jamela2); // ini akan merge empty object dengan object jamela2
+jamelaCopy.lastName = 'Bargouw';
+console.log('before marriage: ', jamela2); // ini akan menampilkan object jamela saat blum menikah
+console.log('after marriage: ', jamelaCopy); // ini akan menampilkan object jamela ketika sudah menikah (lastname nya berubah)
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -2,28 +2,88 @@
 
 // Data needed for a later exercise
 const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+    '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
 const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+    },
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0, // Open 24 hours
+            close: 24,
+        },
     },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
 };
+
+// destructuring object
+const {name, openingHours, categories} = restaurant;
+console.log(name, openingHours, categories);
+
+// mengganti nama
+const {name: restaurantName, openingHours: hours, categories: tags} = restaurant;
+console.log(restaurantName, hours,tags);
+
+//set default value
+const {menu = [], starterMenu: starter = []} = restaurant;
+console.log(menu, starter);
+
+// Mutating variables
+
+
+
+// destructuring array
+// const arr = [2, 3, 4];
+// // meng-assign tiap variable
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+
+// meng-assign data dalam one line
+// const [x, y, z] = arr; // ini disebut destructuring assign, bukan array
+// console.log(x, y, z);
+// variable x valuenya akan berisi value dari index ke 0 di array arr
+
+// const [first, second] = restaurant.categories; // ini hasilnya akan menjadi Italian dan Pizzeria
+// ini akan melompati value kedua dan akan langsung mendapatkan value ketiga
+// let [main, , secondary] = restaurant.categories; // ini hasilnya akan menjadi Italian dan vegetarian
+// console.log(main, secondary);
+//
+// const temp = main;
+// main = secondary; // overWritten
+// secondary = temp;
+// console.log(main, secondary);
+
+// cara lainnya
+// [main, secondary] = [secondary, main]; // lebih singkat, tanpa menggunakan variable temp
+// console.log(main, secondary);
+
+// mendapatkan 2 return values dari function order di object restaurant
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+// dengan nested array
+// const nested = [2,4,[3,2]];
+// // const [i, ,j] = nested; // akan menghasilkan 2 [3,2]
+// // console.log(i,j);
+// const [i, ,[j,k]] = nested;
+// console.log(i,j,k); // akan menghasilkan 2,3,2
+//
+// // const [p,q,r] = [9,1]; // ini akan error, karena panjangnya tidak sama
+// const [p = 1, q = 1, r = 1] = [8,4]; // jika di suatu posisi nilainya tidak di set, maka nilainya menjadi akan 1
+// console.log(p,q,r);
