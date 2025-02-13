ad6131846b7cb4bb97f31c98e2fdfd5a2a1c9aa8
Shifa <luh.shifa@gmail.com>
2025-02-13 18:20:31 +0800

    Thursday, Feb 13 2025

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,6 +3,28 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function (data, className = "") {
+    const html = `
+        <article class="country ${className}">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${+(data.population / 1000000).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+          </div>
+        </article>`
+
+    countriesContainer.insertAdjacentHTML('afterbegin', html);
+    countriesContainer.style.opacity = 1
+}
+
+const renderError = function (msg) {
+    countriesContainer.insertAdjacentText('beforeend', msg);
+    countriesContainer.style.opacity = 1;
+}
+
 ///////////////////////////////////////
 // const getCountryData = function(country) {
 //     const request = new XMLHttpRequest();
@@ -33,22 +55,6 @@ const countriesContainer = document.querySelector('.countries');
 // getCountryData("Portugal");
 // getCountryData("Australia");
 
-const renderCountry = function (data, className = "") {
-    const html = `
-        <article class="country ${className}">
-          <img class="country__img" src="${data.flag}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${+(data.population / 1000000).toFixed(1)}</p>
-            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-          </div>
-        </article>`
-
-    countriesContainer.insertAdjacentHTML('afterbegin', html);
-    countriesContainer.style.opacity = 1
-}
 // const getCountryAndNeighbors = function (country) {
 //     // AJAX call country 1
 //     const request = new XMLHttpRequest();
@@ -102,6 +108,8 @@ const request = fetch('https://restCountries.com/v2/name/usa');
 //
 // };
 
+// consuming promises
+
 // const getCountryData = function (country) {
 //     fetch(`https://restCountries.com/v2/name/${country}`)
 //         .then((response) => response.json())
@@ -109,24 +117,121 @@ const request = fetch('https://restCountries.com/v2/name/usa');
 //
 // };
 
+// consuming promises with chaining promises
+// const getCountryData = function (country) {
+//     // country 1
+//     fetch(`https://restCountries.com/v2/name/${country}`)
+//         .then((response) => response.json())
+//         .then(data => { // promise yang akan menampilkan 23
+//             renderCountry(data[0])
+//             const neighbour = data[0].borders[0]
+//
+//             if(!neighbour) return
+//
+//             // country 2
+//            return fetch(`https://restCountries.com/v2/alpha/${neighbour}`)
+//             // return 23;
+//         })
+//         .then(response => response.json())
+//         .then(data => renderCountry(data, "neighbour"))
+//         // handle with this method, will show alert with value of data
+//         // .then(data => alert(data))
+//
+// };
+// getCountryData("usa");
+
+const getJson = function (url, errMsg = "Something went wrong.") {
+    return fetch(url).then(response => {
+        if (!response.ok) throw new Error(`${errMsg} (${response.status})`);
+        return response.json();
+    })
+}
+
+// handle errors:
+// const getCountryData = function (country) {
+//     // country 1
+//     fetch(`https://restCountries.com/v2/name/${country}`)
+//         .then(response => {
+//             console.log(response);
+//             // jika terjadi/ditemukan adanya error, maka promises akan rejected dan me-return
+//             // dan akan masuk ke dalam code catch di bawah
+//             if (!response.ok) {
+//                 throw new Error(`Country not found (${response.status})`);
+//             }
+//             return response.json()
+//
+//         })
+//         .then(data => { // promise yang akan menampilkan 23
+//             renderCountry(data[0])
+//             const neighbour = data[0].borders[0]
+//
+//             if (!neighbour) return
+//
+//             // country 2
+//             return fetch(`https://restCountries.com/v2/alpha/${neighbour}`)
+//             // return 23;
+//         })
+//         // dipanggil ketika suatu kondisi terpenuhi
+//         .then(response => {
+//             console.log(response)
+//             return response.json()
+//         })
+//         .then(data => renderCountry(data, "neighbour"))
+//         // catching the error, dan dipanggil ketika promises rejected/ditolak (?)
+//         .catch(error => {
+//             console.error(`${error} :(`);
+//             renderError(`Something went wrong, ${error.message} :(. Try again.`);
+//         })
+//         // not always usefully, digunakan ketika ingin mendapatkan result entah itu memenuhi suatu kondisi atau tidak
+//         // contoh case, spinner untuk loading
+//         // only work in promises, atau jika catch juga return promises
+//         .finally(() => {
+//             countriesContainer.style.opacity = 1;
+//         })
+//
+//     // handle with this method, will show alert with value of data
+//     // .then(data => alert(data))
+//
+// };
+
 const getCountryData = function (country) {
     // country 1
-    fetch(`https://restCountries.com/v2/name/${country}`)
-        .then((response) => response.json())
+    getJson(
+        `https://restCountries.com/v2/name/${country}`,
+        "Country not found")
         .then(data => { // promise yang akan menampilkan 23
-            renderCountry(data[0])
-            const neighbour = data[0].borders[0]
+            renderCountry(data[0]);
+            const neighbour = data[0].borders[0];
 
-            if(!neighbour) return
+            if (!neighbour) throw new Error('No neighbour found!');
 
             // country 2
-           return fetch(`https://restCountries.com/v2/alpha/${neighbour}`)
+            return getJson(
+                `https://restCountries.com/v2/alpha/${neighbour}`,
+                "Country not found");
             // return 23;
         })
-        .then(response => response.json())
-        .then(data => renderCountry(data, "neighbour"))
-        // handle with this method, will show alert with value of data
-        // .then(data => alert(data))
+        // catching the error, dan dipanggil ketika promises rejected/ditolak (?)
+        .then(data => renderCountry(data, 'neighbour'))
+        .catch(err => {
+            console.error(`${err} 💥💥💥`);
+            renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+        })
+
+        // not always usefully, digunakan ketika ingin mendapatkan result entah itu memenuhi suatu kondisi atau tidak
+        // contoh case, spinner untuk loading
+        // only work in promises, atau jika catch juga return promises
+        .finally(() => {
+            countriesContainer.style.opacity = 1;
+        })
+
+    // handle with this method, will show alert with value of data
+    // .then(data => alert(data))
 
 };
-getCountryData("usa");
+
+btn.addEventListener('click', function () {
+    getCountryData("vietnam");
+});
+getCountryData("australia");
+
